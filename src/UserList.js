489ce330b54e66a0e31cd78f4152452fcaf08779
hamdibenjarrar <hamdibenjarrar@gmail.com>
2025-02-17 11:48:35 +0100

    Initial commit with API project

---
+++ src/UserList.js
@@ -0,0 +1,34 @@
+import React, { useEffect, useState } from 'react';
+import axios from 'axios';
+
+const UserList = () => {
+  const [listOfUsers, setListOfUsers] = useState([]);
+
+  useEffect(() => {
+    axios.get('https://jsonplaceholder.typicode.com/users')
+      .then(response => {
+        setListOfUsers(response.data);
+      })
+      .catch(error => {
+        console.error('There was an error fetching the users:', error);
+      });
+  }, []);
+
+  return (
+    <div>
+      <h1>List of Users</h1>
+      <ul>
+        {listOfUsers.map(user => (
+          <li key={user.id}>
+            <h2>{user.name}</h2>
+            <p>Email: {user.email}</p>
+            <p>Phone: {user.phone}</p>
+            <p>Website: {user.website}</p>
+          </li>
+        ))}
+      </ul>
+    </div>
+  );
+};
+
+export default UserList;
